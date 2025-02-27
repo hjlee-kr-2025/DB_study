e8f96a7ca5d30c1bb43624ad8e787827c7666f6b
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-02-27 10:44:01 +0900

    250227_2교시F.

--- 250227.sql
+++ 250227.sql
@@ -5,3 +5,15 @@ select @@datadir;
 -- select 는 정보를 가져올때 사용합니다.
 -- select @@ 로 시작하면 DB시스템에 가지고 있는
 -- 정보를 확인할 때 사용하시면 됩니다.
+
+-- 3.1 sakila DB사용
+use sakila;
+-- sakila에 만들어진 table 확인
+show tables;
+-- 데이터 베이스안에 저장된 내용은 테이블이름을 알아야
+-- 가지고 올 수 있습니다.
+
+-- 테이블의 모든 데이터를 불러오자.
+select * from actor;
+select first_name, last_name from actor;
+
